59f3caf407c8dcbcf0ebe176282a4e87edf94f92
Manish Bhattarai <bhaimanish00@gmail.com>
2020-05-20 09:30:53 +0545

    Complete!!

--- src/index.js
+++ src/index.js
@@ -3,16 +3,35 @@ import "./assets/style.css";
 import ReactDOM from "react-dom";
 import quizService from "./quizService";
 import QuestionBox from "./components/QuestionBox";
-
+import Result from "./components/Result";
 class Quiz extends Component {
   state = {
     questionBank: [],
+    score: 0,
+    responses: 0
   };
   getQuestions = () => {
     quizService().then((question) => {
       this.setState({
         questionBank: question,
       });
+    });
+  };
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      this.setState({
+        score: this.state.score +1
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses +1 : 5
+    });
+  };
+  playAgain = () => {
+    this.getQuestions();
+    this.setState({
+      score: 0,
+      responses: 0
     });
   };
   componentDidMount() {
@@ -23,15 +42,19 @@ class Quiz extends Component {
       <div className="container">
         <div className="title">HajiriJawaf</div>
         {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
           this.state.questionBank.map(
             ({ question, answers, correct, questionId }) => (
               <QuestionBox
                 question={question}
                 options={answers} 
                 key={questionId}
+                selected={answer => this.computeAnswer(answer, correct)}
               />
             )
           )}
+
+          {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
       </div>
     );
   }
